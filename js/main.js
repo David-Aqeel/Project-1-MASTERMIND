ed05d3cc7cb954b4aba6a0ad964c1322379a7ac0
DavidAqeel <qbkid123@gmail.com>
2023-04-23 23:03:24 -0400

    pseudocode complete

---
+++ js/main.js
@@ -0,0 +1,38 @@
+//Mastermind Game setup
+
+//Need a home page with the mastermind name top centered in big letter and three menu options: Game Rules, High Score List, and Start Game
+
+//clicking 'Game rules' will explain how to play the game with images of gameplay as well
+
+//clicking 'High score list' will show either the highest score or the 3 highest scores
+
+//clicking 'Start Game' will start the game
+
+//'Game rules' and 'high score list' will be a single static page
+
+//When player clicks 'start game' will be taken to the main game.
+
+
+// game will have the name top center, with the answer hidden behind the name
+
+//still deciding if i should include: 'help' menu will be in the top left with the directions popping up should the player need it.
+
+//score will be in the top right
+
+//timer will start when player makes first pick
+
+//color choices will be on the right side along the border, below which there will be a 'check answer button'
+
+//the player will have 10 chances to guess the code, and there will be 10 lines numbered 10-1 from top to bottom on the left side along the border
+
+//each number will have 4 blank circles to the right of it that will be filled up when the player clicks on a color choice from the color menu.
+
+//the player will have the option to change his choices and so the answer will only be verified when player clicks 'check answer' button
+
+//when player guesses and clicks 'check answer', there will be pegs on the right of his choices that indicate whether he made either a correct color choice (black peg), correct color and placement choice (white peg) or neither(no peg)
+
+//if player guesses the wrong choices, they will get the pegs showing them what they got right, or no pegs if they didnt get anything right
+
+// if player gets the answer right, 'game complete' message will pop up with time taken and points received, and options for a 'new game' or 'back to menu'
+
+//if player gets high score 'new high score' message will pop up
