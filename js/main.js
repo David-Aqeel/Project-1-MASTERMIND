9db807fd6d6668bafb6ae3f7d7000aa561c9cd14
DavidAqeel <qbkid123@gmail.com>
2023-04-27 15:20:21 -0400

    working on comparing the guess to the answer

--- js/main.js
+++ js/main.js
@@ -19,7 +19,9 @@ const COLORS = [
 
 let answer = []
 
+let currentGuess = 0
 
+let currentColumn = 0
 
 let board // array of 4 guesses
 
@@ -34,11 +36,11 @@ console.log('this is checkAnsBtn', checkAnsBtn)
 
 //const playAgainBtn = document.getElementById('playAgain')
 
-const playAgainBtn = document.querySelector('button')
-console.log('this is playAgainBtn', playAgainBtn)
+//const playAgainBtn = document.querySelector('button')
+//console.log('this is playAgainBtn', playAgainBtn)
 
-const boardEls = [...document.querySelectorAll('#board>div')]
-console.log('this is boardEls', boardEls)
+//const boardEls = [...document.querySelectorAll('#board>div')]
+//console.log('this is boardEls', boardEls)
 
 const colorPicks = [...document.querySelectorAll('li')]
 
@@ -47,16 +49,45 @@ const pickColor = (e) => {
     console.log('this is the color')
     console.log(e.target.dataset.color)
 
+    const guessDiv = document.getElementById(`g${currentGuess}c${currentColumn}`)
+    console.log(guessDiv)
+
+    //next steps
+    // now that we can get our target guess div we need to alter it so that it now has the corresponding color 
+
+     function changeColor() {
+        let x = document.getElementById("g0c0");
+        x.style.backgroundColor = "#f40"
+     }
+    
+    // if I have a array to keep track of the current player guess I want to mutate it here
+
+    // then I want to mutate the current column (global) by increasing by 1
+    currentColumn = + 1
+    // if the new current column is 4 i.e row is filled, set it to 0 and increase guess by 1
+    currentGuess = + 1
+    // run guessCheckLogic (compare guess array to answer array)
+
+
+    // give user feedback
+
+
+
+
+
+    //<div id="g0c0"></div>
+    //<div id="g0c1"></div>
+    //<div id="g0c2"></div>
+    //<div id="g0c3"></div>
+
     //I want to have the colors appear in the empty guess slots starting from the bottom left of the array
-    function fillSlot () {
-
-    }
     //I want the colorPicks to populate one row at a time only, from left to right
-
     //I want the user to be able to change their picks by clicking on the populated slot
-
             //When a user picks a color, the first pick will populate the left most column of the lowest unfilled row
-            
+           /* pickColor.addEventListener('click', function fillSlot() {
+                    const fillSlot = e.target.dataset.color
+            });
+            console.log (fillSlot)*/
             // the second pick will populate the spot directly to the right of the first column 
             //the third pick will populate the spot directly to the right of the second column 
             //the fourth pick will populate the spot directly to the right of the third column 
@@ -107,13 +138,15 @@ function getRandomAnswer(answer) {
 }
 console.log(getRandomAnswer(COLORS)) //generating random answer sequence
 
+// store answer const = getRandomAnswer
+
 
 // popup the answer when guessed correctly
 
 
 
 function init() {
-    board =[
+   /* board =[
         [0, 0, 0, 0], // guess 1
         [0, 0, 0, 0], // guess 2
         [0, 0, 0, 0], // guess 3
@@ -125,7 +158,7 @@ function init() {
         [0, 0, 0, 0], // guess 9
         [0, 0, 0, 0], // guess 10
 
-    ];
+    ];*/
     turn = 1;
     winner = null;
     render();
@@ -173,3 +206,17 @@ function renderControls()   {
     //add guess to the guess array.
     //if the guess is wrong and there are fewer than 10 guesses continue to the next guess, otherwise end game and show score/reset game button.
     //give feedback by manipulating dom.
+
+
+    // render code
+    // on button click 
+    //first check the board (js)
+    // inside the board, see if the first subarray has any open positions
+    // if the first array includes 0s, find the first 0 and replace it
+    // replace it with the color picked
+    // then update the background color for the guess element
+
+    //checkans
+
+    // on submit go through the board array, find the first array that has 0 0s.
+    // once found, loop through every getRandomAnswer array and compare each index of the guess with the computer generated ans.
