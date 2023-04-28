23778503f507a8a4b3b4c5afe77abee2e1cd8b16
DavidAqeel <qbkid123@gmail.com>
2023-04-28 09:03:12 -0400

    game

---
+++ Planning.md
@@ -0,0 +1,142 @@
+# Project-1-MASTERMIND
+Wireframes:
+
+![Homepage Wireframe](https://github.com/David-Aqeel/Project-1-MASTERMIND/blob/response/imgs/Wireframe1.jpg)
+![Gameplay Wireframe](https://github.com/David-Aqeel/Project-1-MASTERMIND/blob/response/imgs/Wireframe2.jpg)
+
+
+
+PSEUDOCODE:
+
+    //Start by making an empty guess array.
+    //cache dom elements
+    //generate an answer to compare with the user guesses.
+    //player can start guessing via dom interface.
+    //add guess to the guess array. (START WITH 1 GUESS AND WHEN THE CODE IS FUNCTIONAL JUST ADD GUESSES (li) TO AN INCREASING LIST (ol)).
+    //if the guess is wrong and there are fewer than 10 guesses continue to the next guess, otherwise end game and show score/reset game button.
+    //give feedback by manipulating dom.
+
+--- 
+
+/*----- constants -----*/
+//Color choices- Hold the values of the color choices 
+//Check Answer Button
+
+/*----- state variables -----*/
+//guess slots - 10 arrays with 4 nested arrays
+//correct guess vs incorrect guess
+
+/*----- cached elements  -----*/
+//color choices
+//play again button
+//game board
+//check answer button
+//empty color slots (black)
+
+
+/*----- event listeners -----*/
+//button clicks
+//correct answer
+
+
+///Upon game loading the page shoud:
+/// -Initialize state variables:
+        >Setup empty guess slots(4 nested arrays)/render board
+        >Show color options on the side
+        >Have an answer generated (that is hidden) to match the guesses with
+
+---
+//Mastermind Game setup
+
+-game board:
+    ```js
+    let board
+    ```
+
+
+//Need a home page with the mastermind name top centered in big letter and three menu options: Game Rules, High Score List, and Start Game
+
+//clicking 'Game rules' will explain how to play the game with images of gameplay as well
+
+//clicking 'High score list' will show either the highest score or the 3 highest scores
+
+//clicking 'Start Game' will start the game
+
+//#can 'Game rules' and 'high score list' be modals?###
+
+//When player clicks 'start game' will be taken to the main game.
+
+// manipulate dom to go to game view.
+
+// game will have the name top center, with the answer hidden behind the name
+
+//still deciding if i should include: 'help' menu will be in the top left with the directions popping up should the player need it.
+
+//score will be in the top right
+
+//timer will start when player makes first pick/            STRETCH
+
+//color choices will be on the right side along the border, below which there will be a 'check answer button'.
+
+//the player will have 10 chances to guess the code, and there will be 10 lines numbered 10-1 from top to bottom on the left side along the border
+    //Start by making an empty guess array.
+    //cache dom elements
+    //generate an answer to compare with the user guesses.
+    //player can start guessing via dom interface.
+    //add guess to the guess array. (START WITH 1 GUESS AND WHEN THE CODE IS FUNCTIONAL JUST ADD GUESSES (li) TO AN INCREASING LIST (ul)).
+    //if the guess is wrong and there are fewer than 10 guesses continue to the next guess, otherwise end game and show score/reset game button.
+    //give feedback by manipulating dom.
+
+//each number will have 4 blank circles to the right of it that will be filled up when the player clicks on a color choice from the color menu.
+
+//the player will have the option to change his choices and so the answer will only be verified when player clicks 'check answer' button
+
+//when player guesses and clicks 'check answer', there will be pegs on the right of his choices that indicate whether he made either a correct color choice (black peg), correct color and placement choice (white peg) or neither(no peg)
+
+//if player guesses the wrong choices, they will get the pegs showing them what they got right, or no pegs if they didnt get anything right
+
+// if player gets the answer right, 'game complete' message will pop up with time taken and points received, and options for a 'new game' or 'back to menu'
+
+//if player gets high score 'new high score' message will pop.      STRETCH
+
+
+
+
+/*//Mastermind Game setup
+
+//Need a home page with the mastermind name top centered in big letter and three menu options: Game Rules, High Score List, and Start Game
+
+//clicking 'Game rules' will explain how to play the game with images of gameplay as well
+
+//clicking 'High score list' will show either the highest score or the 3 highest scores
+
+//clicking 'Start Game' will start the game
+
+//'Game rules' and 'high score list' will be a single static page
+
+//When player clicks 'start game' will be taken to the main game.
+
+
+// game will have the name top center, with the answer hidden behind the name
+
+//still deciding if i should include: 'help' menu will be in the top left with the directions popping up should the player need it.
+
+//score will be in the top right
+
+//timer will start when player makes first pick
+
+//color choices will be on the right side along the border, below which there will be a 'check answer button'
+
+//the player will have 10 chances to guess the code, and there will be 10 lines numbered 10-1 from top to bottom on the left side along the border
+
+//each number will have 4 blank circles to the right of it that will be filled up when the player clicks on a color choice from the color menu.
+
+//the player will have the option to change his choices and so the answer will only be verified when player clicks 'check answer' button
+
+//when player guesses and clicks 'check answer', there will be pegs on the right of his choices that indicate whether he made either a correct color choice (black peg), correct color and placement choice (white peg) or neither(no peg)
+
+//if player guesses the wrong choices, they will get the pegs showing them what they got right, or no pegs if they didnt get anything right
+
+// if player gets the answer right, 'game complete' message will pop up with time taken and points received, and options for a 'new game' or 'back to menu'
+
+//if player gets high score 'new high score' message will pop*/
--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
 
-//PSEUDOCODE:
-/*----- constants -----*/
-//Mastermind logo
-//Color choices
 const COLORS = [
     'red',
     'orange',
@@ -12,37 +8,25 @@ const COLORS = [
     'purple'
 ]
 
-//Check Answer Button
+function createAns() {
+    answer = [] 
+    for (let i = 0; i < 4; i++) { 
+        answer.push(COLORS[Math.floor(Math.random()* COLORS.length)])
+    }
+    console.log(answer)
+}
 
-/*----- state variables -----*/
-//guess slots
-
-let answer = []
+function getRandomAnswer(answer) {
+    return [...answer].sort(() => Math.random () > 0.5 ? 1 : -1).slice(0,4)
+}
+console.log(getRandomAnswer(COLORS))
 
 let currentGuess = 0
 
 let currentColumn = 0
 
-let board // array of 4 guesses
-
-
-let winner // null || 1 || -1
-
-/*----- cached elements  -----*/
-//color choices
-
 const checkAnsBtn = document.getElementById('checkAns')
 console.log('this is checkAnsBtn', checkAnsBtn)
-
-//const playAgainBtn = document.getElementById('playAgain')
-
-//const playAgainBtn = document.querySelector('button')
-//console.log('this is playAgainBtn', playAgainBtn)
-
-//const boardEls = [...document.querySelectorAll('#board>div')]
-//console.log('this is boardEls', boardEls)
-
-const colorPicks = [...document.querySelectorAll('li')]
 
 const pickColor = (e) => {
 
@@ -52,121 +36,35 @@ const pickColor = (e) => {
     const guessDiv = document.getElementById(`g${currentGuess}c${currentColumn}`)
     console.log(guessDiv)
 
-    //next steps
-    // now that we can get our target guess div we need to alter it so that it now has the corresponding color 
-
-     function changeColor() {
+}
+ function changeColor ( y ) {
         let x = document.getElementById("g0c0");
-        x.style.backgroundColor = "#f40"
-     }
-    
-    // if I have a array to keep track of the current player guess I want to mutate it here
+        x.style.backgroundColor = y
 
-    // then I want to mutate the current column (global) by increasing by 1
-    currentColumn = + 1
-    // if the new current column is 4 i.e row is filled, set it to 0 and increase guess by 1
-    currentGuess = + 1
-    // run guessCheckLogic (compare guess array to answer array)
+   
 
-
-    // give user feedback
-
-
-
-
-
-    //<div id="g0c0"></div>
-    //<div id="g0c1"></div>
-    //<div id="g0c2"></div>
-    //<div id="g0c3"></div>
-
-    //I want to have the colors appear in the empty guess slots starting from the bottom left of the array
-    //I want the colorPicks to populate one row at a time only, from left to right
-    //I want the user to be able to change their picks by clicking on the populated slot
-            //When a user picks a color, the first pick will populate the left most column of the lowest unfilled row
-           /* pickColor.addEventListener('click', function fillSlot() {
-                    const fillSlot = e.target.dataset.color
-            });
-            console.log (fillSlot)*/
-            // the second pick will populate the spot directly to the right of the first column 
-            //the third pick will populate the spot directly to the right of the second column 
-            //the fourth pick will populate the spot directly to the right of the third column 
-            //when all 4 spots in a row are filled the user cannot fill any more spots unless:
-                //the user clicks on an already picked color to unselect it
-                //the user clicks the check answer button to verify their guess with (getRandomAnswer(COLORS))
-            //the user should only be able to populate one row at a time before clicking checkAnsBtn
-            //if the answer is correct, the user will get a "winner" message
-            //if the answer is wrong, the user will move on to their next guess (next row)
-            //if the user guesses 10 times and gets the answer wrong, the game will end, and the play again button will pop up.
-
-
-    
-}
- 
-colorPicks.forEach(colorLi => {
-    colorLi.addEventListener('click', pickColor)
-}) 
-
-
-function changeColor () {
+    colorPicks.forEach(colorLi => {
+        colorLi.addEventListener('click', pickColor)
+    }) 
 
 }
 
-/*console.log('this is colorPicks', colorPicks)
-colorPicks.forEach(color => {
-    console.log()
-})*/
 
-/*----- event listeners -----*/
-//button clicks
-//document.getElementById('red').addEventListener('click', playerGuess)
-
-//correct answer
-
-
-/*----- functions -----*/
-//pick colors
-//check answers
-//compare guess to answer
-
-function createAns() {
-    //i want this function to pick 4 random options out of the 6 available options and store that as the answer
-    answer = [] // make sure answer array is empty at the beginning of game
-    for (let i = 0; i < 4; i++) { //looping 4 times 
-        answer.push(COLORS[Math.floor(Math.random()* COLORS.length)]) // picking one random color option and pushing it to 'answer'
-    }
-    console.log(answer)
-}
-
-function getRandomAnswer(answer) {
-    return [...answer].sort(() => Math.random () > 0.5 ? 1 : -1).slice(0,4)//sorting through the color options and removing already selected colors and 
-}
-console.log(getRandomAnswer(COLORS)) //generating random answer sequence
-
-// store answer const = getRandomAnswer
-
-function changeColor ( y ) {
-    let x = document.getElementById("g0c0");
-    x.style.backgroundColor = y
-}
-
-
-// popup the answer when guessed correctly
 
 
 
 function init() {
    board =[
-        [0, 0, 0, 0], // guess 1
-        [0, 0, 0, 0], // guess 2
-        [0, 0, 0, 0], // guess 3
-        [0, 0, 0, 0], // guess 4
-        [0, 0, 0, 0], // guess 5
-        [0, 0, 0, 0], // guess 6
-        [0, 0, 0, 0], // guess 7
-        [0, 0, 0, 0], // guess 8
-        [0, 0, 0, 0], // guess 9
-        [0, 0, 0, 0], // guess 10
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0], 
 
     ];
     turn = 1;
@@ -175,12 +73,8 @@ function init() {
 }
 
 function render() {
-    //render board
     renderBoard()
-    //render buttons
     renderControls()
-    //render message
-
 }
 
 
@@ -208,26 +102,4 @@ function renderControls()   {
  function playerGuess(event)    {
         const colIdx = boardEls.indexOf(event.target)
  }
-    //Start by making an empty guess array.
-    //let guess_1 = [];
-    //cache dom elements
-
-    //generate an answer to compare with the user guesses.
-    //player can start guessing via dom interface.
-    //add guess to the guess array.
-    //if the guess is wrong and there are fewer than 10 guesses continue to the next guess, otherwise end game and show score/reset game button.
-    //give feedback by manipulating dom.
-
-
-    // render code
-    // on button click 
-    //first check the board (js)
-    // inside the board, see if the first subarray has any open positions
-    // if the first array includes 0s, find the first 0 and replace it
-    // replace it with the color picked
-    // then update the background color for the guess element
-
-    //checkans
-
-    // on submit go through the board array, find the first array that has 0 0s.
-    // once found, loop through every getRandomAnswer array and compare each index of the guess with the computer generated ans.
+   
