5992864eb217f7862d3e719522fa0cb854eb922d
DavidAqeel <qbkid123@gmail.com>
2023-05-05 03:28:14 -0400

    cleaned up code

--- js/main.js
+++ js/main.js
@@ -20,66 +20,44 @@ computerGuessArray = getRandomAnswer(COLORS)
 console.log('This is answer', computerGuessArray)
 
 
-
-
-
-// save the answers as a variable
-
-// const = randmAnswer = getRandomAnswer(COLORS)
-// if userAns === computerGuessArray then popup winner message
-// if userAns !== computerGuessArray then move to the next guess/row (current++)
-
-
 let currentGuess = 0
 
 let currentColumn = 0
 
 let checkWinCount 
 
-//const playAgainBtn = document.querySelector('button')
-// console.log('this is playAgainBtn', playAgainBtn)
-
 let userGuessArray = []
 
 
 function pickColor(e) {
 
-        const selectedColor = e.target.dataset.color;
-        const guessDiv = document.getElementById(`g${currentGuess}c${currentColumn}`);
-        guessDiv.style.backgroundColor = selectedColor;
-        // increment currentColumn to move to the next guess
-        currentColumn++;
-        console.log(selectedColor);
-        if (userGuessArray.length === 4) {
-            userGuessArray = []
-        }
-        // else if (userGuessArray.length !== 4 && e.target.id === "g9c3") {
-                
-        // }
-        userGuessArray.push(selectedColor);
-       moveUpRow();
-        return selectedColor;
+    const selectedColor = e.target.dataset.color;
+    const guessDiv = document.getElementById(`g${currentGuess}c${currentColumn}`);
+    guessDiv.style.backgroundColor = selectedColor;
+    currentColumn++;
+    console.log(selectedColor);
+    if (userGuessArray.length === 4) {
+        userGuessArray = []
+    }
+  
+    userGuessArray.push(selectedColor);
+    moveUpRow();
+    return selectedColor;
 
 }
 
-//make switching rows its own function: move up row
 function moveUpRow() {
     if (currentColumn === 4) {
         compareArrays()
-        // reset currentColumn to 0 and increment currentGuess
         currentColumn = 0;
         currentGuess++;
         if (currentGuess >= 10 && userGuessArray[i] !== computerGuessArray[i]) {
             document.querySelector('#you-lose').style.display="flex"   
         }
-        // TODO: handle switching to the next guess
     }    
     console.log(userGuessArray)
 }
 
-//create a function to render out the player array everytime it moves up a row 
-//when a row is complete(user guess array has 4 elements), compare the user array with the computer guess array(nested if loop)(if guess array = 4 then do a for loop with an if statement)
-//if userGuessArr matches computerGuessArr, then render winner message, else move to next row and clear out the user guess array. Can move the moveUpRow function into this statement for checking win condition.
 
 function compareArrays() {
     let matches = 0
@@ -94,8 +72,6 @@ function compareArrays() {
                 console.log('We have a match')
                 matches++
                 tryAgain.innerHTML = "Amazing.";
-
-                // make a check win count variable and if checkWinCount = 4 then you have a winner. render win message
 
             } else {
                 console.log("We don't have a match")
@@ -118,73 +94,8 @@ function compareArrays() {
         userGuessArray = []
     }
 }
-// let a1 = document.querySelector('#g0c0').style.backgroundColor
-// let a2 = document.querySelector('#g0c1')
-// let a3 = document.querySelector('#g0c2')
-// let a4 = document.querySelector('#g0c3')
 
 
-
-//compareArray list:
-// if the user array matches (if matches === 4) declare a winner
-// if the two arrays don't match clear out the user guess array so we only get a maximum of  4 items in the array
-//if the two arrays don't match move on to the next guess
-//if all 10 guesses are wrong, declare lose msg
-//need to track if user guesses are incorrect (state variable) and how many incorrect guesses.
-
-
-
-
-
-//needs a counter
-//make a clear array function so that after each check of 4 we will clear the array
-//call clear array function in your else statement in the compare array function
-
-
-
-// function 
-// if(userGuessArr.length === 4) {
-//     let isCorrect = userGuessArr.every((color, index) => {
-//         return color === computerGuessArray[index];
-//     });
-// }
-
-
-// if (isCorrect) {
-//     console.log('Winner!')
-// }
-// else {
-//     console.log
-// }
-
-
-// log all of the computers guesses into its own array. compare that array with the users guesses
-// use the .every method:
-// user guess. every =>. 
-
-//console.log(userGuessArray)
-
-/*function pickColor(e) {
-    const selectedColor = e.target.dataset.color;
-    const guessDiv = document.getElementById(`g${currentGuess}c${currentColumn}`);
-    guessDiv.style.backgroundColor = selectedColor;
-    // increment currentColumn to move to the next guess
-    currentColumn++;
-    console.log(selectedColor);
-    return selectedColor;
-    userGuessArr.push(selectedColor)
-}*/
-
-
-// currentGuess++ for changing the guess
-
-
-  //for each guess, add the 4 picks to an empty array and then compare that with the answer,
-  //if it is wrong then move on to the next guess, and if it is right then render the winner message. 
-
-
-
-// add the guess to a list and compare with answer
 
 
 function changeColor ( i ) {
@@ -198,56 +109,3 @@ changeColor()
 
 
 
-function init() {
-   board =[
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0], 
-
-    ];
-    checkWinCount = 0;
-
-    winner = null;
-    render();
-}
-
-
-
-// function render() {
-//     renderBoard()
-//     renderControls()
-// }
-
-
-// function renderBoard() {
-//     board.forEach((colArr, colIdx) => {
-//     console.log(colArr, colIdx)
-//         colArr.forEach((cellVal, rowIdx) => {
-//         console.log(cellVal, rowIdx)
-//         const cellId = `r${rowIdx}c${colIdx}`
-//         const cellEl = document.getElementById(cellId)
-//         cellEl.style.backgroundColor = COLORS[cellVal]
-    
-//         })
-//     })
-// }
-
-
-// function renderControls()   {
-//     playAgainBtn.style.visibility = winner ?'visible' : 'hidden'
-
-//     boardEls.forEach((boardEl, colIdx) => {
-//         const hideMarker = !board[colIdx].include(0) || winner 
-//         boardEl.style.visibility = hideBoard ? 'hidden' : 'visible'
-//     })
-// }
-//  function playerGuess(event)    {
-//         const colIdx = boardEls.indexOf(event.target)
-//  }
